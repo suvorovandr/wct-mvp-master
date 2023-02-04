8483545035a7bd5967ffe0e0361022133935e68b
AS <suvorov.andrey2015@gmail.com>
2023-02-04 12:50:40 +0300

    insert dublicate issue fixed. Mechta has dublicate

--- main_kz.py
+++ main_kz.py
@@ -5,7 +5,6 @@ import requests
 import csv
 import os.path
 
-
 store_domain = "https://www.mechta.kz/api/v1/product/"
 catalog_link = "https://www.mechta.kz/api/v1/catalog?section=smartfony&page=1&properties=&page_limit=24&cache_city=s1"
 
@@ -40,24 +39,26 @@ def get_links_to_db():
     db_create()
     get_catalog_jsons = requests.get(catalog_link)
     json = get_catalog_jsons.json()
-    device_count = json['data'].get('all_items_count')
-    print("All need to parse: "+ str(device_count) + " devices")
+    ## device_count = json['data'].get('all_items_count')
     page_count = json['data'].get('page_items_count')
-    device_sum = 0
-    for i in range(page_count + 1):
+    for i in range(1, page_count + 1):
+        print("page: " + str(i))
         catalog_link_auto = "https://www.mechta.kz/api/v1/catalog?section=smartfony&page="+str(i)+"&properties=&page_limit="+str(page_count)+"&cache_city=s1"
+        ##print(catalog_link_auto)
         items_json = requests.get(catalog_link_auto).json()
         device_code = items_json['data'].get('items')
-        for i in range(len(device_code)):
+        ##print(len(device_code))
+        for i in range(len(device_code) - 1):
+            ##print(type(device_code[i]))
             link_attribut = device_code[i]['code']
+            device_id = device_code[i]['id']
+            print(str(device_id) + " | " + link_attribut)
             full_link_attribut = "https://www.mechta.kz/product/" + link_attribut
             db_write_data(full_link_attribut)
             ## print(type(device_code[i]))
             ##for i, code in enumerate(items_dic):
                     ##code[i] = items_dic[0]
                    ## print(code['code'])
-            device_sum = device_sum + 1
-    print(device_sum)
 
 print("Welcome to use Honor WCT Kazakhstan prototype")
 print("Author: Andrey Suvorov (C) Honor Devices")
@@ -79,15 +80,13 @@ def parse_page(addr_list):
         parse_index += 1
         print("already parsed " + str(parse_index) + " / " + str(len(addr_list)) + " devices", end ='\r')
         url = store_domain + default_addr[30:]
-        page = requests.get(url)
-        market_data = page.json()
+        market_data = requests.get(url).json()
         ## market_data = page.json()
         device_name = market_data['data'].get('name')
         ids = market_data['data'].get('id')
         api = "https://www.mechta.kz/api/v1/mindbox/actions/product"
         headers = {'Content-type': 'application/json'}
-        page_json = requests.post(api, headers=headers, json={'product_ids': ids})
-        json_data = page_json.json()
+        json_data = requests.post(api, headers=headers, json={'product_ids': ids}).json()
         base_price = json_data['data'].get('prices').get('base_price')
         discounted_price = json_data['data'].get('prices').get('discounted_price')
         bonus = json_data['data'].get('bonus')
@@ -95,7 +94,7 @@ def parse_page(addr_list):
         gift_array = []
         if gift_data == True:
             gift_object = json_data['data'].get('gifts')
-            ## print(gift_object)
+            print(type(gift_object))
             key = list(gift_object.keys())
             for i in range(len(key)):
                 gift_details = gift_object.get(key[i])
@@ -103,16 +102,16 @@ def parse_page(addr_list):
                     keys[i] = gift_details[0]
                     gift_name = keys['name']
                     gift_array.append(gift_name)
-                    gift = requests.post(api, headers=headers, json={'product_ids': keys['id']})
-                    gift_json = gift.json()
+                    gift_json = requests.post(api, headers=headers, json={'product_ids': keys['id']}).json()
                 try:
                     gift_base_price = gift_json['data'].get('prices').get('base_price')
                     ## print(device_name, base_price, discounted_price, bonus, gift_array, gift_base_price)
                     data = [device_name, base_price, discounted_price, bonus, gift_array, gift_base_price]
                     writer.writerow(data)
                 except(AttributeError): continue
-        data = [device_name, base_price, discounted_price, bonus, gift_array]
-        writer.writerow(data)
+        else: 
+            data = [device_name, base_price, discounted_price, bonus, gift_array]
+            writer.writerow(data)
     print("CSV file already Ok :)                                             ")
             ##data = [device_name, base_price, discounted_price, bonus, gift_array]
             ##write_file.writerow(data)
